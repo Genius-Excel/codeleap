24174dcf764ca235c0807288e3a48dec072e3ed9
Genius-Excel <geniusexcel9@gmail.com>
2026-03-05 14:56:18 +0100

    Fix health check URL by adding trailing slash

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('health', views.health_check, name='health-check'),
+    path('health/', views.health_check, name='health-check'),
+    path('', views.posts_collection, name='posts-collection'),
+    path('<int:post_id>/', views.post_update, name='post-update'),
 ]
