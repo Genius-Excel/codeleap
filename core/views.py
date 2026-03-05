18145c21ba4379c855d9c164de8f9dd39a87c8e0
Genius-Excel <geniusexcel9@gmail.com>
2026-03-05 14:34:55 +0100

    Refactor views.py to import necessary modules for future API development

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
-# Create your views here.
+from .models import Post
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 def health_check(request):
     return JsonResponse({"message": "Health check successful!"})
