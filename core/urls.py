56d8b1bae62e00e75ad26ab3c1eadecb67ebb5c3
Genius-Excel <geniusexcel9@gmail.com>
2026-03-05 16:18:27 +0100

    Rename URL pattern for posts to improve clarity: posts-collection to posts-list-create

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('health/', views.health_check, name='health-check'),
-    path('', views.retrieve_and_create_posts, name='posts-collection'),
+    path('', views.retrieve_and_create_posts, name='posts-list-create'),
     path('<int:post_id>/', views.update_or_delete_post, name='post-update'),
 ]
