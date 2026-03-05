5e23a8a63e0c6089910751ef2f129c67cd2320ee
Genius-Excel <geniusexcel9@gmail.com>
2026-03-05 14:34:50 +0100

    Add PostSerializer to handle Post model serialization

---
+++ core/serializers.py
@@ -0,0 +1,11 @@
+from .models import Post
+from rest_framework import serializers
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'username', 'title', 'content', 'created_datetime']
+        read_only_fields = ['id', 'created_datetime']
+
+ 
